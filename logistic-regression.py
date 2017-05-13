6bdcc2968149aa307e9cc28e4d86d179189e80ed
AlMikFox3 <sasmit1997@outlook.com>
2017-05-14 00:33:43 +0530

    Initial Commit.

---
+++ logistic-regression.py
@@ -0,0 +1,110 @@
+from random import seed
+from random import randrange
+from csv import reader
+from math import sqrt, exp
+
+def load_csv(filename):
+	dataset = list()
+	with open(filename, 'r') as f:
+		csv_reader = reader(f)
+		for row in csv_reader:
+			if not row:
+				continue
+			else:
+				dataset.append(row)
+	return dataset
+
+def str_column_to_float(dataset,column):
+	for row in dataset:
+		row[column] = float(row[column].strip())
+
+def dataset_minmax(dataset):
+	minmax = list()
+	for i in range(len(dataset[0])):
+		col_values = [row[i] for row in dataset]
+		value_min = min(col_values)
+		value_max = max(col_values)
+		minmax.append([value_min, value_max])
+	return minmax
+
+def normalize_dataset(dataset, minmax):
+	for row in dataset:
+		for i in range(len(row)):
+			row[i] = (row[i] - minmax[i][0])/(minmax[i][1] - minmax[i][0])
+
+def cross_validation_split(dataset, n_folds):
+	dataset_split = list()
+	dataset_copy = list(dataset)
+	fold_size = int(len(dataset) / n_folds)
+	for i in range(n_folds):
+		fold = list()
+		while len(fold) < fold_size:
+			index = randrange(len(dataset_copy))
+			fold.append(dataset_copy.pop(index))
+		dataset_split.append(fold)
+	return dataset_split
+
+def accuracy_metric(actual, predicted):
+	correct = 0
+	for i in range(len(actual)):
+		if actual[i] == predicted[i]:
+			correct += 1
+	return correct / float(len(actual)) * 100.0
+
+def evaluate_algorithm(dataset, algorithm, n_folds, *args):
+	folds = cross_validation_split(dataset, n_folds)
+	scores = list()
+	for fold in folds:
+		train_set = list(folds)
+		train_set.remove(fold)
+		train_set = sum(train_set, [])
+		test_set = list()
+		for row in fold:
+			row_copy = list(row)
+			test_set.append(row_copy)
+			row_copy[-1] = None
+		predicted = algorithm(train_set, test_set, *args)
+		actual = [row[-1] for row in fold]
+		accuracy = accuracy_metric(actual, predicted)
+		scores.append(accuracy)
+	return scores
+
+def predict(row, coefficients):
+	yhat = coefficients[0]
+	for i in range(len(row)-1):
+		yhat += coefficients[i + 1] * row[i]
+	return 1.0 / (1.0 + exp(-yhat))
+
+def coefficients_sgd(train, l_rate, n_epoch):
+	coef = [0.0 for i in range(len(train[0]))]
+	for epoch in range(n_epoch):
+		for row in train:
+			yhat = predict(row, coef)
+			error = row[-1] - yhat
+			coef[0] = coef[0] + l_rate * error * yhat * (1.0 - yhat)
+			for i in range(len(row)-1):
+				coef[i + 1] = coef[i + 1] + l_rate * error * yhat * (1.0 - yhat) * row[i]
+	return coef
+
+def logistic_regression(train, test, l_rate, n_epoch):
+	predictions = list()
+	coef = coefficients_sgd(train, l_rate, n_epoch)
+	for row in test:
+		yhat = predict(row, coef)
+		yhat = round(yhat)
+		predictions.append(yhat)
+	return(predictions)
+
+seed(1)
+filename = 'pima-indians-diabetes.csv'
+dataset = load_csv(filename)
+for i in range(len(dataset[0])):
+	str_column_to_float(dataset, i)
+minmax = dataset_minmax(dataset)
+normalize_dataset(dataset, minmax)
+n_folds = 5
+l_rate = 0.1
+n_epoch = 100
+scores = evaluate_algorithm(dataset, logistic_regression, n_folds, l_rate, n_epoch)
+print('Scores: %s' % scores)
+print('Mean Accuracy: %.3f%%' % (sum(scores)/float(len(scores))))
